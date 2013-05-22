4c8cc097b689083c68255bba3a2b173ac48682ca
emre <mail@emreyilmaz.me>
2013-05-22 12:31:06 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='stormssh',
-    version='0.2.2',
+    version='0.2.3',
     packages=['storm'],
     url='http://github.com/emre/storm',
     license='MIT',
