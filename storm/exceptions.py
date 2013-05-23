3654a97cab4924975b32b85b597fdcfa5ee26587
benvand <benjamin_vandersteen@hotmail.com>
2013-05-23 10:04:37 +1000

    Added new uinparsable line exception Added handling for host * entry

--- storm/exceptions.py
+++ storm/exceptions.py
@@ -3,3 +3,9 @@
 
 class StormValueError(ValueError):
     pass
+
+class NoHostname(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return "Could not parse host: "+repr(self.value)+'\tNo Hostname entry'
