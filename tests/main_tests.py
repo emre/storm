e9bc661b8f86736ae83d9e1614db67e897a14939
Jeune Asuncion <jeune.asuncion@applovin.com>
2014-04-24 21:46:51 -0700

    reorganize tests folder in lieu of storm#53

--- tests/main_tests.py
+++ tests/main_tests.py
@@ -10,9 +10,9 @@ s.get_formatted_message.return_value = ""
 
 class StormMainTest(unittest.TestCase):
     def test_udpate(self): 
-        Storm.update_entry = mock.MagicMock()
-        s.update("google")
-        Storm.update_entry.assert_called_with("google")
+        with mock.patch('storm.Storm.update_entry') as update_entry:
+            s.update("google")
+            update_entry.assert_called_with("google")
 
 if __name__ == '__main__':
     unittest.main()
