58719a44766a92328dde27a2f73b181d6b711c46
Emre Yilmaz <mail@emreyilmaz.me>
2014-07-21 17:26:23 +0300

    initial CLI tests

--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@ import os
 import shlex
 import subprocess
 import sys
+from mock import patch
+
 try:
     import unittest2 as unittest
 except ImportError:
@@ -15,30 +17,283 @@ import six
 from storm import Storm
 from storm.ssh_uri_parser import parse
 from storm.exceptions import StormInvalidPortError, StormValueError
+from storm import __version__
+
+
+# derived from http://www.cyberciti.biz/faq/create-ssh-config-file-on-linux-unix/
+FAKE_SSH_CONFIG_FOR_CLI_TESTS = """
+    ### default for all ##
+    Host *
+         ForwardAgent no
+         ForwardX11 no
+         ForwardX11Trusted yes
+         User nixcraft
+         Port 22
+         Protocol 2
+         ServerAliveInterval 60
+         ServerAliveCountMax 30
+         LocalForward 3128 127.0.0.1:3128
+         LocalForward 3129 127.0.0.1:3128
+
+    ## override as per host ##
+    Host server1
+         HostName server1.cyberciti.biz
+         User nixcraft
+         Port 4242
+         IdentityFile /nfs/shared/users/nixcraft/keys/server1/id_rsa
+         IdentityFile /tmp/x.rsa
+
+    ## Home nas server ##
+    Host nas01
+         HostName 192.168.1.100
+         User root
+         IdentityFile ~/.ssh/nas01.key
+
+    ## Login AWS Cloud ##
+    Host aws.apache
+         HostName 1.2.3.4
+         User wwwdata
+         IdentityFile ~/.ssh/aws.apache.key
+
+    ## Login to internal lan server at 192.168.0.251 via our public uk office ssh based gateway using ##
+    ## $ ssh uk.gw.lan ##
+    Host uk.gw.lan uk.lan
+         HostName 192.168.0.251
+         User nixcraft
+         ProxyCommand  ssh nixcraft@gateway.uk.cyberciti.biz nc %h %p 2> /dev/null
+
+    ## Our Us Proxy Server ##
+    ## Forward all local port 3128 traffic to port 3128 on the remote vps1.cyberciti.biz server ##
+    ## $ ssh -f -N  proxyus ##
+    Host proxyus
+        HostName vps1.cyberciti.biz
+        User breakfree
+        IdentityFile ~/.ssh/vps1.cyberciti.biz.key
+        LocalForward 3128 127.0.0.1:3128
+"""
 
 
 class StormCliTestCase(unittest.TestCase):
 
+    def setUp(self):
+        self.config_file = '/tmp/ssh_config_cli_tests'
+        with open(self.config_file, 'w+') as f:
+            f.write(FAKE_SSH_CONFIG_FOR_CLI_TESTS)
+
+        self.config_arg = '--config={}'.format(self.config_file)
+
     def run_cmd(self, cmd):
+
         cmd = 'storm %s' % cmd
         cmd = shlex.split(cmd.encode('utf-8') if six.PY2 else cmd)
+        _env = os.environ
+        _env["TESTMODE"] = "1"
+
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
+                                   stderr=subprocess.PIPE,
+                                   env=_env)
         out, err = process.communicate()
         rc = process.returncode
         return out, err, rc
 
     def test_list_command(self):
-        out, err, rc = self.run_cmd('list')
-        self.assertEqual(out, b'\x1b[37mlisting entries:\n\n\x1b[0m\n')
+        out, err, rc = self.run_cmd('list {}'.format(self.config_arg))
+
+        self.assertTrue(out.startswith(" Listing entries:\n\n"))
+
+        hosts, custom_options = [
+            "aws.apache -> wwwdata@1.2.3.4:22",
+            "nas01 -> root@192.168.1.100:22",
+            "proxyus -> breakfree@vps1.cyberciti.biz:22",
+            "server1 -> nixcraft@server1.cyberciti.biz:4242",
+            "uk.gw.lan uk.lan -> nixcraft@192.168.0.251:22",
+        ], [
+            "[custom options] identityfile=~/.ssh/aws.apache.key",
+            "[custom options] identityfile=~/.ssh/nas01.key",
+            "[custom options] identityfile=~/.ssh/vps1.cyberciti.biz.key localforward=3128 127.0.0.1:3128",
+            "[custom options] identityfile=/nfs/shared/users/nixcraft/keys/server1/id_rsa,/tmp/x.rsa",
+            "[custom options] proxycommand=ssh nixcraft@gateway.uk.cyberciti.biz nc %h %p 2> /dev/null",
+        ]
+
+        general_options = {
+            "forwardx11": "no",
+            "protocol": "2",
+            "user": "nixcraft",
+            "forwardagent": "no",
+            "forwardx11trusted": "yes",
+            "serveralivecountmax": "30",
+            "serveraliveinterval": "60",
+            "port": "22",
+            "localforward": "3128 127.0.0.1:3128, 3129 127.0.0.1:3128",
+        }
+
+        for host in hosts:
+            self.assertIn(host, out)
+
+        for custom_option in custom_options:
+            self.assertIn(custom_option, out)
+
+        for general_option, value in general_options.iteritems():
+            self.assertIn("{}: {}".format(general_option, value), out)
+
         self.assertEqual(err, b'')
         self.assertEqual(rc, 0)
+
+    def test_version_command(self):
+        out, err, rc = self.run_cmd('version')
+        self.assertIn(__version__, out)
+
+    def test_basic_add(self):
+        out, err, rc = self.run_cmd('add netscaler ns@42.42.42.42 {}'.format(self.config_arg))
+
+        self.assertIn("success", out)
+
+    def test_add_duplicate(self):
+        out, err, rc = self.run_cmd('add aws.apache test@test.com {}'.format(self.config_arg))
+
+        self.assertEqual(b'', out)
+        self.assertIn('error', err)
+
+    def test_add_invalid_host(self):
+        out, err, rc = self.run_cmd('add @_@ test.com {}'.format(self.config_arg))
+
+        self.assertEqual(b'', out)
+        self.assertIn('error', err)
+
+    def test_advanced_add(self):
+        out, err, rc = self.run_cmd('add postgresql-server postgres@192.168.1.1 {} {}{}'.format(
+            "--id_file=/tmp/idfilecheck.rsa ",
+            '--o "StrictHostKeyChecking=yes" --o "UserKnownHostsFile=/dev/advanced_test" ',
+            self.config_arg)
+        )
+
+        self.assertIn("success", out)
+
+        with open(self.config_file) as f:
+            # check that property is really flushed out to the config?
+            content = f.read()
+            self.assertIn("identityfile /tmp/idfilecheck.rsa", content)
+            self.assertIn("stricthostkeychecking yes", content)
+            self.assertIn("userknownhostsfile /dev/advanced_test", content)
+
+    def test_add_with_idfile(self):
+        out, err, rc = self.run_cmd('add postgresql-server postgres@192.168.1.1 {} {}'.format(
+            "--id_file=/tmp/idfileonlycheck.rsa",
+            self.config_arg)
+        )
+
+        self.assertIn("success", out)
+
+        with open(self.config_file) as f:
+            content = f.read()
+            self.assertIn("identityfile /tmp/idfileonlycheck.rsa", content)
+
+    def test_basic_edit(self):
+        out, err, rc = self.run_cmd('edit aws.apache basic_edit_check@10.20.30.40 {}'.format(self.config_arg))
+
+        self.assertIn("success", out)
+
+        with open(self.config_file) as f:
+            content = f.read()
+            self.assertIn("basic_edit_check", content)
+            self.assertIn("10.20.30.40", content)
+
+    def test_edit_invalid_host(self):
+        out, err, rc = self.run_cmd('edit @missing_host test.com {}'.format(self.config_arg))
+
+        self.assertEqual(b'', out)
+        self.assertIn('error', err)
+
+    def test_edit_invalid_host(self):
+        out, err, rc = self.run_cmd('edit missing_host test.com {}'.format(self.config_arg))
+
+        self.assertEqual(b'', out)
+        self.assertIn('error', err)
+
+    def test_update(self):
+        out, err, rc = self.run_cmd('update aws.apache --o "user=daghan" --o port=42000 {}'.format(self.config_arg))
+
+        self.assertIn("success", out)
+
+        with open(self.config_file) as f:
+            content = f.read()
+            self.assertIn("user daghan", content)  # see daghan: http://instagram.com/p/lfPMW_qVja
+            self.assertIn("port 42000", content)
+
+    def test_update_regex(self):
+
+        self.run_cmd('add worker alphaworker.com {}'.format(self.config_arg))
+
+        # add three machines -- hostnames starts with worker-N
+        self.run_cmd('add worker-1 worker1.com {}'.format(self.config_arg))
+        self.run_cmd('add worker-2 worker2.com {}'.format(self.config_arg))
+        self.run_cmd('add worker-4 worker4.com {}'.format(self.config_arg))
+
+        # another one -- regex shouldn't capture that one though.
+        self.run_cmd('add worker3 worker3.com {}'.format(self.config_arg))
+
+        out, err, rc = self.run_cmd("update 'worker-[1-5]' --o hostname=boss.com {}".format(self.config_arg))
+
+        self.assertIn("success", out)
+
+        # edit the alphaworker
+        out, err, rc = self.run_cmd('edit worker alphauser@alphaworker.com {}'.format(self.config_arg))
+
+        with open(self.config_file) as f:
+            content = f.read()
+            self.assertNotIn("worker1.com", content)
+            self.assertNotIn("worker2.com", content)
+            self.assertNotIn("worker4.com", content)
+            self.assertIn("worker3.com", content)
+            self.assertIn("alphauser", content)
+
+        out, err, rc = self.run_cmd("edit worker  {}".format(self.config_arg))
+
+    def test_update_invalid_regex(self):
+
+        out, err, rc = self.run_cmd("update 'drogba-[0-5]' --o hostname=boss.com {}".format(self.config_arg))
+
+        self.assertEqual(b'', out)
+        self.assertIn('error', err)
+
+    def test_delete(self):
+        out, err, rc = self.run_cmd("delete server1 {}".format(self.config_arg))
+        self.assertIn("success", out)
+
+    def test_delete_invalid_hostname(self):
+
+        out, err, rc = self.run_cmd("delete unknown_server".format(self.config_arg))
+
+        self.assertEqual(b'', out)
+        self.assertIn('error', err)
+
+    def test_search(self):
+
+        out, err, rc = self.run_cmd("search aws {}".format(self.config_arg))
+
+        self.assertTrue(out.startswith('Listing results for aws:'))
+        self.assertIn('aws.apache', out)
+
+    def test_invalid_search(self):
+
+        out, err, rc = self.run_cmd("search THEREISNOTHINGRELATEDWITHME {}".format(self.config_arg))
+
+        self.assertIn('no results found.', out)
+
+    def test_delete_all(self):
+        out, err, rc = self.run_cmd('delete_all {}'.format(self.config_arg))
+
+        self.assertIn('all entries deleted', out)
+
+
+    def tearDown(self):
+        os.unlink('/tmp/ssh_config_cli_tests')
 
 
 class StormTests(unittest.TestCase):
 
     def setUp(self):
-        FAKE_SSH_CONFIG = """Host *
+        fake_ssh_config = """Host *
             IdentitiesOnly yes
 
         Host netscaler
@@ -48,7 +303,7 @@ class StormTests(unittest.TestCase):
         """
 
         with open('/tmp/ssh_config', 'w+') as f:
-            f.write(FAKE_SSH_CONFIG)
+            f.write(fake_ssh_config)
 
         self.storm = Storm('/tmp/ssh_config')
 
@@ -163,8 +418,8 @@ class StormTests(unittest.TestCase):
 
         for item in self.storm.ssh_config.config_data:
             if item.get("host") == 'host_with_custom_option':
-                self.assertEqual(item.get("options").get("StrictHostKeyChecking"), 'no')
-                self.assertEqual(item.get("options").get("UserKnownHostsFile"), '/dev/null')
+                self.assertEqual(item.get("options").get("stricthostkeychecking"), 'no')
+                self.assertEqual(item.get("options").get("userknownhostsfile"), '/dev/null')
 
         custom_options = (
             "StrictHostKeyChecking=yes",
@@ -177,8 +432,8 @@ class StormTests(unittest.TestCase):
 
         for item in self.storm.ssh_config.config_data:
             if item.get("host") == 'host_with_custom_option':
-                self.assertEqual(item.get("options").get("StrictHostKeyChecking"), 'yes')
-                self.assertEqual(item.get("options").get("UserKnownHostsFile"), '/home/emre/foo')
+                self.assertEqual(item.get("options").get("stricthostkeychecking"), 'yes')
+                self.assertEqual(item.get("options").get("userknownhostsfile"), '/home/emre/foo')
 
     def tearDown(self):
         os.unlink('/tmp/ssh_config')
