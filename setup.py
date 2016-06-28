cbce90e97f84fd88acfa36a5b6aa1e46cc3b7752
Emre Yilmaz <emre.yilmaz@metglobal.com>
2016-06-28 10:58:31 +0300

    Version bump to 0.6.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stormssh',
-    version='0.6.8',
+    version='0.6.9',
     packages=find_packages(),
     package_data={'storm': ['templates/*.html', 'static/css/*.css',
                             'static/css/themes/storm/*.css', 'static/css/themes/storm/img/*.png',
--- storm/__init__.py
+++ storm/__init__.py
@@ -10,7 +10,7 @@ from .parsers.ssh_config_parser import ConfigParser
 from .defaults import get_default
 
 
-__version__ = '0.6.8'
+__version__ = '0.6.9'
 
 ERRORS = {
     "already_in": "{0} is already in your sshconfig. "
