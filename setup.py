cc10c0da45ef553fb9c19dc13cefaa7cbe3ac3ab
Daniele Esposti <daniele.esposti@gmail.com>
2019-10-29 09:04:07 +0000

    Updated classifiers

--- setup.py
+++ setup.py
@@ -35,9 +35,10 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
         'Topic :: System :: Systems Administration',
     ]
